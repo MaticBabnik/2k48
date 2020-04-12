d8e4e9684a3d9439c122453c19ecc6e8e016f0b8
Matic Babnik <matic.babnik1@gmail.com>
2020-04-12 17:51:07 +0200

    Revert "release 2"
    This reverts commit 42d61300ab2f712e5cfb36a4a41f5db9b8e8df68.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("2k48")]
-[assembly: AssemblyDescription("shitty 2048")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Matic Babnik")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("2k48")]
-[assembly: AssemblyCopyright("Matic Babnik")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
